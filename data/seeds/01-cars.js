767d7d67863601ac552c8f871d2c0e7ba7553f9e
jillig3 <jacobillig@gmail.com>
2021-11-03 16:20:26 -0500

    cars js

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,23 @@
-// STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },    
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 1500,
+    },
+]
