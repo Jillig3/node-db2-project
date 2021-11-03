a71bab77399aabf6be823945d583f541b19243e1
jillig3 <jacobillig@gmail.com>
2021-11-03 16:28:41 -0500

    server.js

--- api/server.js
+++ api/server.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const server = express();
 
-// DO YOUR MAGIC
 const carsRouter = require('./cars/cars-router.js');
 server.use(express.json());
+
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'not found'})
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
 server.unsubscribe('/api/cars', carsRouter);
+
 module.exports = server;
